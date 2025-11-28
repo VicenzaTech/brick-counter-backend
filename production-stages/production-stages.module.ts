0cbd6d84acdfb0fff2380d0839a3107dfd672a50
nguyen hieu <hieubait99@gmail.com>
2025-11-28 13:01:57 +0700

    chore

--- production-stages/production-stages.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProductionStagesService } from './production-stages.service';
-import { ProductionStagesController } from './production-stages.controller';
-
-@Module({
-  providers: [ProductionStagesService],
-  controllers: [ProductionStagesController]
-})
-export class ProductionStagesModule {}
